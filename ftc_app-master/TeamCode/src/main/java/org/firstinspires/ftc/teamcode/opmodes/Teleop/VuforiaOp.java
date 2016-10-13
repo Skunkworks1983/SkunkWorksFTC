5739da80228b5f25d3ea4279f6fbc7c0cdf87ebc
rizoph <rizoph@gmail.com>
2016-10-12 19:49:33 -0700

    Fixed le thing

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/VuforiaOp.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/VuforiaOp.java
@@ -1,16 +1,17 @@
 package org.firstinspires.ftc.teamcode.opmodes.Teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.vuforia.HINT;
 import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.teamcode.R;
 
 
 public class VuforiaOp extends LinearOpMode {
@@ -18,7 +19,7 @@ public class VuforiaOp extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        params.vuforiaLicenseKey = "";
+        params.vuforiaLicenseKey = "AZkqQkH/////AAAAGdiGT5c3sUI4rBa3mJU4jt18f+jlyz2znmOo2EiHBSTv77Q5ujdYwemMmLblk51L+dDswjbz3BwJtlNdupU7ee5LMwuE+pRmJhJSTfzmHsL5+z9enLQv88uQj7yGuy1WCfKDQS7lNh6FMlRoswyIfH3aPc9ncQFgk4Hk22gOosnpA6ugrbrqKzg802X4INkGq/ozNtt/RdR/xW0KfMFNRpiNX5VwvjV6mgx2i6XuRfAemjeCmcansRUsdpy54RmrwdH57krn48/L9xAouVvNK+6Boq8PXo+OB0jTngIo0JCWmr58T7qwW2b27EL6FSdoOFbd94hjzfnRvZilROP9IwQULyYgbqKZWyAvym/dzwYT";
         params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
 
         VuforiaLocalizer vuforia = ClassFactory.createVuforiaLocalizer(params);
@@ -33,20 +34,21 @@ public class VuforiaOp extends LinearOpMode {
         waitForStart();
 
         beacons.activate();
-        while(opModeIsActive()) {
-            for(VuforiaTrackable beac : beacons ) {
-                OpenGLMatrix pose =((VuforiaTrackableDefaultListener) beac.getListener()).getPose();
+        while (opModeIsActive()) {
+            for (VuforiaTrackable beac : beacons) {
+                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) beac.getListener()).getPose();
 
-                if(pose !=null){
+                if (pose != null) {
                     VectorF translation = pose.getTranslation();
 
                     telemetry.addData(beac.getName() + "~Translation", translation);
 
-                    double degreesToTurn = Math.toDegrees(Math.atan2(translation.get(1). translation.get(2)));
+                    double degreesToTurn = Math.toDegrees(Math.atan2(translation.get(1), translation.get(2)));
 
-                    telemetry.addData(beac.getName() +"~Degrees", degreesToTurn);
+                    telemetry.addData(beac.getName() + "~Degrees", degreesToTurn);
                 }
             }
             telemetry.update();
         }
     }
+}
