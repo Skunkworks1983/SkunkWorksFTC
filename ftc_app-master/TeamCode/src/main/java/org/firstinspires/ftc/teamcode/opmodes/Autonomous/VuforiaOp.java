c72dfee1a48bbdae60699813119450743c143cd5
roads0 <roads@roads.pw>
2016-10-17 18:42:58 -0700

    meme
    1

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autonomous/VuforiaOp.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autonomous/VuforiaOp.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes.Autonomous;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.vuforia.HINT;
 import com.vuforia.Vuforia;
 
@@ -10,16 +8,11 @@ import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.teamcode.R;
 
-/**
- * Created by JON WICK on 10/12/2016.
- */
-
-@Autonomous(name="VuforiaTutorial", group="Vuforia")  // @Autonomous(...) is the other common choice
 
 public class VuforiaOp extends LinearOpMode {
     @Override
@@ -41,23 +34,21 @@ public class VuforiaOp extends LinearOpMode {
         waitForStart();
 
         beacons.activate();
-
-        while (opModeIsActive()){
-            for(VuforiaTrackable beac : beacons){
+        while (opModeIsActive()) {
+            for (VuforiaTrackable beac : beacons) {
                 OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) beac.getListener()).getPose();
 
-                if(pose != null){
+                if (pose != null) {
                     VectorF translation = pose.getTranslation();
 
-                    telemetry.addData(beac.getName() + "-Translation", translation);
+                    telemetry.addData(beac.getName() + "~Translation", translation);
 
                     double degreesToTurn = Math.toDegrees(Math.atan2(translation.get(1), translation.get(2)));
 
-                    telemetry.addData(beac.getName() + "-Degrees", degreesToTurn);
-
+                    telemetry.addData(beac.getName() + "~Degrees", degreesToTurn);
                 }
             }
+            telemetry.update();
         }
-        telemetry.update();
     }
 }
