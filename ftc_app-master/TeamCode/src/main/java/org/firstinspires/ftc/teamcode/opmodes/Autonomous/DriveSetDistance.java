7d9e668476474d8f9cceaf41cf340bdd574a922c
jeremywboyle <jeremywboyle@gmail.com>
2016-11-02 19:41:23 -0700

    hi

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autonomous/DriveSetDistance.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autonomous/DriveSetDistance.java
@@ -134,7 +134,7 @@ public class DriveSetDistance extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  4,  -4, 3.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(DRIVE_SPEED,  10,  -10, 3.0);  // S1: Forward 47 Inches with 5 Sec timeout
         //encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
         //encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
 
