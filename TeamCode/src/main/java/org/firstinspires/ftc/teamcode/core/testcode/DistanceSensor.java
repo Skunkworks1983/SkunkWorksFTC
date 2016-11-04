3dca02787d53ef844d93ee7c2b7948328ab1b4ba
DietCookiez <diet@dietcookiez.me>
2016-11-04 16:25:18 -0700

    Couple Sensors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/testcode/DistanceSensor.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.core.testcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
+/**
+ * Created by Adam.
+ * November 04, 2016 at 3:33 PM
+ */
+
+/*
+    Using hardware spot "A7"
+ */
+@TeleOp(name = "Distance Sensor", group = "Sensor")
+public class DistanceSensor extends LinearOpMode
+{
+
+    OpticalDistanceSensor odsSensor;  // Hardware Device Object
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+
+        // get a reference to our Light Sensor object.
+        odsSensor = hardwareMap.opticalDistanceSensor.get("ods");
+        odsSensor.enableLed(true);
+
+        // wait for the start button to be pressed.
+        waitForStart();
+
+        // while the op mode is active, loop and read the light levels.
+        // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
+        while (opModeIsActive())
+        {
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("Raw", odsSensor.getRawLightDetected());
+            telemetry.addData("Normal", odsSensor.getLightDetected());
+
+            telemetry.update();
+            idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
+        }
+    }
+}
+
