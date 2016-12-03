d592fcabacbed4b63249994b44248bffe8a16674
roads0 <roads@roads.pw>
2016-12-03 14:32:53 -0800

    vuforia is kinda off fbut it works

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autonomous/TestingEncoderDrive.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autonomous/TestingEncoderDrive.java
@@ -3,15 +3,22 @@ package org.firstinspires.ftc.teamcode.opmodes.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.RobotLog;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.teamcode.opmodes.Base;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by s-4041730 on 12/2/2016.
@@ -20,6 +27,18 @@ import org.firstinspires.ftc.teamcode.opmodes.Base;
 @Autonomous(name="encoderdrivinghple")
 public class TestingEncoderDrive extends ConceptVuforiaNavigation{
 
+    public static final String TAG = "Vuforia Sample";
+
+    //vuforia
+    float x;
+    float y;
+    float z;
+    float robotBearing;
+    Orientation rot;
+    OpenGLMatrix lastLocation = null;
+    VuforiaLocalizer vuforia;
+
+    //gyro and drive
     double DRIVE_SPEED = 0.75;
     private ElapsedTime runtime = new ElapsedTime(); //put this in every thing which you want a runtime
     int target = -45;
@@ -27,32 +46,127 @@ public class TestingEncoderDrive extends ConceptVuforiaNavigation{
     @Override
     public void runOpMode() throws InterruptedException {
         init(hardwareMap);
-        vuforiaInit();
 
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(com.qualcomm.ftcrobotcontroller.R.id.cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = "AZkqQkH/////AAAAGdiGT5c3sUI4rBa3mJU4jt18f+jlyz2znmOo2EiHBSTv77Q5ujdYwemMmLblk51L+dDswjbz3BwJtlNdupU7ee5LMwuE+pRmJhJSTfzmHsL5+z9enLQv88uQj7yGuy1WCfKDQS7lNh6FMlRoswyIfH3aPc9ncQFgk4Hk22gOosnpA6ugrbrqKzg802X4INkGq/ozNtt/RdR/xW0KfMFNRpiNX5VwvjV6mgx2i6XuRfAemjeCmcansRUsdpy54RmrwdH57krn48/L9xAouVvNK+6Boq8PXo+OB0jTngIo0JCWmr58T7qwW2b27EL6FSdoOFbd94hjzfnRvZilROP9IwQULyYgbqKZWyAvym/dzwYT";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        VuforiaTrackables beacons = vuforia.loadTrackablesFromAsset("FTC_2016-17");
+
+        VuforiaTrackable wheels = beacons.get(0);
+        VuforiaTrackable tools = beacons.get(1);
+        VuforiaTrackable lego = beacons.get(2);
+        VuforiaTrackable gears = beacons.get(3);
+
+        wheels.setName("Wheels");
+        tools.setName("Tools");
+        lego.setName("Lego");
+        gears.setName("Gears");
+
+        List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
+        allTrackables.addAll(beacons);
+
+        float mmPerInch        = 25.4f;
+        float mmBotWidth       = 17 * mmPerInch;            // ... or whatever is right for your robot
+        float mmFTCFieldWidth  = (12*12 - 2) * mmPerInch;   // the FTC field is ~11'10" center-to-center of the glass panels
+
+        OpenGLMatrix wheelsTargetLocationOnField = OpenGLMatrix
+                /* Then we translate the target off to the RED WALL. Our translation here
+                is a negative translation in X.*/
+                .translation(mmFTCFieldWidth/300, mmFTCFieldWidth/1790, 0)
+                .multiplied(Orientation.getRotationMatrix(
+                        /* First, in the fixed (field) coordinate system, we rotate 90deg in X, then 90 in Z */
+                        AxesReference.EXTRINSIC, AxesOrder.XZX,
+                        AngleUnit.DEGREES, 90, -90, 0));
+        wheels.setLocation(wheelsTargetLocationOnField);
+        RobotLog.ii(TAG, "Wheels Target=%s", format(wheelsTargetLocationOnField));
+
+       /*
+        * To place the Stones Target on the Blue Audience wall:
+        * - First we rotate it 90 around the field's X axis to flip it upright
+        * - Finally, we translate it along the Y axis towards the blue audience wall.
+        */
+        OpenGLMatrix toolsTargetLocationOnField = OpenGLMatrix
+                /* Then we translate the target off to the Blue Audience wall.
+                Our translation here is a positive translation in Y.*/
+                .translation(-mmFTCFieldWidth/1790, mmFTCFieldWidth/900, 0)
+                .multiplied(Orientation.getRotationMatrix(
+                        /* First, in the fixed (field) coordinate system, we rotate 90deg in X */
+                        AxesReference.EXTRINSIC, AxesOrder.XZX,
+                        AngleUnit.DEGREES, 90, -90, 0));
+        tools.setLocation(toolsTargetLocationOnField);
+        RobotLog.ii(TAG, "Tools Target=%s", format(toolsTargetLocationOnField));
+
+        OpenGLMatrix legoTargetLocationOnField = OpenGLMatrix
+                /* Then we translate the target off to the RED WALL. Our translation here
+                is a negative translation in X.*/
+                .translation(-mmFTCFieldWidth/900, mmFTCFieldWidth/1790, 0)
+                .multiplied(Orientation.getRotationMatrix(
+                        /* First, in the fixed (field) coordinate system, we rotate 90deg in X, then 90 in Z */
+                        AxesReference.EXTRINSIC, AxesOrder.XZX,
+                        AngleUnit.DEGREES, 0, -90, 0));
+        lego.setLocation(legoTargetLocationOnField);
+        RobotLog.ii(TAG, "Lego Target=%s", format(legoTargetLocationOnField));
+
+       /*
+        * To place the Stones Target on the Blue Audience wall:
+        * - First we rotate it 90 around the field's X axis to flip it upright
+        * - Finally, we translate it along the Y axis towards the blue audience wall.
+        */
+        OpenGLMatrix gearsTargetLocationOnField = OpenGLMatrix
+                /* Then we translate the target off to the Blue Audience wall.
+                Our translation here is a positive translation in Y.*/
+                .translation(-mmFTCFieldWidth/1790, -mmFTCFieldWidth/300, 0)
+                .multiplied(Orientation.getRotationMatrix(
+                        /* First, in the fixed (field) coordinate system, we rotate 90deg in X */
+                        AxesReference.EXTRINSIC, AxesOrder.XZX,
+                        AngleUnit.DEGREES, 0, -90, 0));
+        gears.setLocation(gearsTargetLocationOnField);
+        RobotLog.ii(TAG, "Gears Target=%s", format(gearsTargetLocationOnField));
+
+        OpenGLMatrix phoneLocationOnRobot = OpenGLMatrix
+                .translation(mmBotWidth/2,0,0)
+                .multiplied(Orientation.getRotationMatrix(
+                        AxesReference.EXTRINSIC, AxesOrder.YZY,
+                        AngleUnit.DEGREES, -90, 0, 0));
+        RobotLog.ii(TAG, "phone=%s", format(phoneLocationOnRobot));
+
+        ((VuforiaTrackableDefaultListener)wheels.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
+        ((VuforiaTrackableDefaultListener)tools.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
+        ((VuforiaTrackableDefaultListener)lego.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
+        ((VuforiaTrackableDefaultListener)gears.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
+
+        telemetry.addData(">", "Press Play to start tracking");
+        telemetry.update();
         waitForStart();
+
+        beacons.activate();
 
         resetEncoders();
         runUsingEncoders();
         encoderDrive(DRIVE_SPEED, 10, 10, 5.0);
 
         while (opModeIsActive()){
-
-            runWithoutEncoders();
-            turnAbsolute(target);
             
             for (VuforiaTrackable trackable : allTrackables) {
-
-                telemetry.addData(trackable.getName(), ((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible() ? "Visible" : "Not Visible");
+                /**
+                 * getUpdatedRobotLocation() will return null if no new information is available since
+                 * the last time that call was made, or if the trackable is not currently visible.
+                 * getRobotLocation() will return null if the trackable is not currently visible.
+                 */
+                telemetry.addData(trackable.getName(), ((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible() ? "Visible" : "Not Visible");    //
 
                 OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
                 if (robotLocationTransform != null) {
                     lastLocation = robotLocationTransform;
                 }
             }
-
+            /**
+             * Provide feedback as to where the robot was last located (if we know).
+             */
             if (lastLocation != null) {
                 //  RobotLog.vv(TAG, "robot=%s", format(lastLocation));
-
                 float[] robotLocationArray = lastLocation.getData();
                 x = robotLocationArray[12];
                 y = robotLocationArray[13];
@@ -62,11 +176,17 @@ public class TestingEncoderDrive extends ConceptVuforiaNavigation{
 
                 robotBearing = rot.thirdAngle;
 
-                telemetry.addData("Turning", "");
-                telemetry.addData("X", x + " " + "Y", y);
+                telemetry.addData("Location", "X" + x + " " + "Y" + y);
                 telemetry.addData("Rotation", robotBearing);
+
+            } else {
+                telemetry.addData("Pos", "Unknown");
             }
             telemetry.update();
+
+            runWithoutEncoders();
+            turnAbsolute(target);
+
         }
     }
 }
