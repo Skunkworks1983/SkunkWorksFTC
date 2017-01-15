9f8e14a1ffcd969d225890d0da1986be8c8a83d5
SplitPixl <splitpixl@gmail.com>
2017-01-15 08:29:14 -0800

    Syncing latest changes from Floobits

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TotallyOurAutonomous.java
@@ -0,0 +1,152 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.teamcode.opmodes.Base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mikes on 2016-10-04.
+ * OpMode to find the Gears target and drive towards it in autonomous.
+ * stop when distance < 400mm.
+ * Note: seems to require the bot to be within about 5 feet, any further and it can't find the target
+ */
+
+@Autonomous(name = "The Good Vuphoia", group = "vision")
+public class TotallyOurAutonomous extends Base {
+
+    OpenGLMatrix pose;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        double left = 0;
+        double right = 0;
+
+        VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
+        params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        params.vuforiaLicenseKey = "ATnP8EP/////AAAAGcfzu5cnFUpOg1NOut+hsxBWPS05nbLtESnR77c9pS0gKqYn3ill1m4oavCEkEBIqgOnYBVp1Vsi5QPMPeXoDFG9ijcRA1ipvPSCUtTxg7i9s9Y6CkjyfGB6o7GWgD4cu+Jg6HQ2PkcC98eIBfMlIpJk3XQY7KONQ7U+VOIk8I/i8+j0iby/nQMdz9nBB7UjywXtXtGmzzYIJysgV3swj2ow9vCNrF5nZDoNJvLQw4y7h9eKCvaLxrk5QIN1lwZt9IRlciR2D/gKiodv8oyKteR+8vjyG5i5Q5iUwIMF6PctEIBM4I6cUZx8oBM9MjfwyjnbOeQWtatu5w3jTf4uTOB77Uy3P0VoQuTXw0JERPrT";
+        params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
+
+        VuforiaLocalizer vuforia = ClassFactory.createVuforiaLocalizer(params);
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);   // four targets on the field
+
+        VuforiaTrackables beacons = vuforia.loadTrackablesFromAsset("FTC_2016-17");
+        beacons.get(0).setName("Wheels");
+        beacons.get(1).setName("Tools");
+        beacons.get(2).setName("Lego");
+        beacons.get(3).setName("Gears");
+
+        List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
+        allTrackables.addAll(beacons);
+
+        telemetry.addData("Vuforia", "Vuforia Initialized");
+        telemetry.update();
+
+        telemetry.addData(">", "Gyro Calibrating. Do Not move!");
+        telemetry.update();
+        init(hardwareMap);
+        sleep(1000);
+        mrGyro.calibrate();
+
+        while (mrGyro.isCalibrating()) { //Ensure calibration is complete (usually 2 seconds)
+            idle();
+        }
+        telemetry.addData(">", "Gyro Calibrated.  Press Start.");
+        telemetry.update();
+
+        waitForStart();
+
+        resetEncoders();
+        runUsingEncoders();
+
+        encoderDrive(0.35, -46, -46, 5.0);
+
+        runWithoutEncoders();
+        turnAbsolute(-90);
+
+        beacons.activate();  // start tracking beacons
+
+        while (opModeIsActive()) {
+
+            for (VuforiaTrackable trackable : allTrackables) {
+
+                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) trackable.getListener()).getPose();  // get gears target position
+
+                if (pose != null) {
+                    if (((VuforiaTrackableDefaultListener) trackable.getListener()).isVisible()) {
+                        telemetry.addData(trackable.getName() + "-visible", "Yes");
+                    } else {
+                        telemetry.addData(trackable.getName() + "-visible", "No");  // does not seem to be invokved once target found once.
+                    }
+                    VectorF translation = pose.getTranslation();
+                    //telemetry.addData(beacon.getName() + "-Translation", translation); // format(pose) shows both orientation and translation
+                    //telemetry.addData(beacon.getName()+"-vector", pose.toVector());    // dump entire matrix
+
+                    telemetry.addData("Pose", format(pose));
+                    /* based on observation of pose, it represents the position and orientation of the target image.
+                       The position is in milimeters, the orientation is of the target, not the phone.
+                       eg. if the phone and target are both parallel to the floor, but the phone is offset towards one edge of the target
+                       and is above the floor,
+                       then the orientation is (0,0,0) but the translation will contain an offset from the centre of the target,
+                       in both the vertical axis and in the other axis.
+                       It's that offset that can be used to calculate the angle from the phone to the target.
+                     */
+
+                    double angle = Math.atan2(translation.get(2), translation.get(0)); // in radians
+                    double degreesToTurn = Math.toDegrees(angle) + 90;                 // adjust for vertical orientation of phone
+                    telemetry.addData(trackable.getName() + "-Degrees", degreesToTurn);
+                    double distance = Math.sqrt(translation.get(2) * translation.get(2) + translation.get(0) * translation.get(0));  // Pythagoras calc of hypotenuse
+                    telemetry.addData(trackable.getName() + "-Distance", distance);
+
+                    if (distance < 1000) { //if we're within 4 cm, stop so the color sensor can read
+                        left = 0;
+                        right = 0;
+                    } else if (degreesToTurn > 10) {  //turn right
+                        left = -0.3;
+                        right = -0.1;
+                    } else if (degreesToTurn < -10) { ///turn left
+                        left = -0.1;
+                        right = -0.3;
+                    } else {  // go toward the target
+                        left = -0.4;
+                        right = -0.4;
+                    }
+
+                    telemetry.addData("Left Power: ", left);
+                    telemetry.addData("Right Power: ", right);
+
+                    MotorFL.setPower(left);
+                    MotorBL.setPower(left);
+                    MotorFR.setPower(right);
+                    MotorBR.setPower(right);
+                    idle();
+
+                }
+                telemetry.update();
+                idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
+            }
+        }
+    }
+
+    /**
+     * A simple utility that extracts positioning information from a transformation matrix
+     * and formats it in a form palatable to a human being.
+     */
+    String format(OpenGLMatrix transformationMatrix) {
+        return transformationMatrix.formatAsTransform();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/GyroWorkPlease.java
@@ -0,0 +1,121 @@
+package org.firstinspires.ftc.teamcode.opmodes.Teleop;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+
+/**
+ * Created by s-4041730 on 1/9/2017.
+ */
+
+@TeleOp(name = "eeeeee")
+public class GyroWorkPlease extends LinearOpMode{
+
+
+    int buttonPressA = 0; //Helps ensure the button was pressed only once
+    int buttonPressB = 0;
+
+    DcMotor mr1;
+    DcMotor mr2;
+    DcMotor ml2;
+    DcMotor ml1;
+    ModernRoboticsI2cGyro mrGyro;
+    GyroSensor sensorGyro;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        mr1 = hardwareMap.dcMotor.get("rightFront");
+        mr2 = hardwareMap.dcMotor.get("rightBack");
+        ml1 = hardwareMap.dcMotor.get("leftFront");
+        ml2 = hardwareMap.dcMotor.get("leftBack");
+        ml1.setDirection(DcMotor.Direction.FORWARD);
+        ml2.setDirection(DcMotor.Direction.FORWARD);
+        mr1.setDirection(DcMotor.Direction.REVERSE);
+        mr2.setDirection(DcMotor.Direction.REVERSE);
+
+        ml1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        ml2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        mr2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        mr1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        int angleZ = 0;
+        int target = 0;
+        sensorGyro = hardwareMap.gyroSensor.get("gyro");
+        mrGyro = (ModernRoboticsI2cGyro) sensorGyro;
+
+        telemetry.addData(">", "Gyro Calibrating. Do Not move!");
+        telemetry.update();
+        mrGyro.calibrate();
+
+        while (mrGyro.isCalibrating()) { //Ensure calibration is complete (usually 2 seconds)
+            idle();
+        }
+        telemetry.addData(">", "Gyro Calibrated.  Press Start.");
+        telemetry.update();
+
+        waitForStart();
+        while (opModeIsActive()) {
+            angleZ = mrGyro.getIntegratedZValue();
+            telemetry.addData(">1", angleZ);
+            if (gamepad1.a)
+                buttonPressA++;
+            if (gamepad1.b)
+                buttonPressB++;
+            if(buttonPressA == 1)
+                target = target + 45;
+            if(buttonPressB == 1)
+                target = target - 45;
+            telemetry.addData(">2", target);
+            telemetry.update();
+
+            turnAbsolute(target);
+
+        }
+    }
+
+        public void turnAbsolute(int target) throws InterruptedException {
+            int zAccumulated = mrGyro.getHeading();  //Set variables to gyro readings
+            double turnSpeed = 0.5;
+            //double slowdownSpeed = 0;
+
+            while (Math.abs(zAccumulated - target) > 3) {  //Continue while the robot direction is further than three degrees from the target
+                if (zAccumulated > target) {  //if gyro is positive, we will turn right
+                    ml1.setPower(turnSpeed);
+                    ml2.setPower(turnSpeed);
+                    mr1.setPower(-turnSpeed);
+                    mr2.setPower(-turnSpeed);
+                }
+
+                if (zAccumulated < target) {  //if gyro is positive, we will turn left
+                    ml1.setPower(-turnSpeed);
+                    ml2.setPower(-turnSpeed);
+                    mr1.setPower(turnSpeed);
+                    mr2.setPower(turnSpeed);
+                }
+
+                if (Math.abs(zAccumulated - target) > 30) {
+                    turnSpeed = 0.5;
+                } else {
+                    turnSpeed = 0.2;
+                }
+
+                zAccumulated = mrGyro.getHeading();  //Set variables to gyro readings
+                telemetry.addData("Turn Speed", turnSpeed);
+                //telemetry.addData("1. accu", String.format("%03d", zAccumulated));
+                telemetry.addData("Heading", zAccumulated);
+                telemetry.addData("Target", target);
+                telemetry.update();
+                idle();
+            }
+
+            ml1.setPower(0);
+            ml2.setPower(0);
+            mr2.setPower(0);
+            ml2.setPower(0);
+            buttonPressA = 0;
+            buttonPressB = 0;
+        }
+}
